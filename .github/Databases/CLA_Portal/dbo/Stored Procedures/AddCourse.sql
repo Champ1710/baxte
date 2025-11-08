3b91de6e11b6d2de97ea377a929a26661a8609d8
Champ1710 <157827935+Champ1710@users.noreply.github.com>
2025-11-08 15:10:59 +0530

    Add test stored procedure for TEST deployment

---
+++ .github/Databases/CLA_Portal/dbo/Stored Procedures/AddCourse.sql
@@ -0,0 +1,23 @@
+-- ============================================
+-- Author:      Chandana Priya
+-- Create date: 2025-11-06
+-- Description: Sample stored procedure for testing deployment
+-- ============================================
+
+IF OBJECT_ID('dbo.AddCourse', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.AddCourse;
+GO
+
+CREATE PROCEDURE dbo.AddCourse
+    @CourseName NVARCHAR(100),
+    @CreatedBy NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO dbo.Courses (CourseName, CreatedBy, CreatedDate)
+    VALUES (@CourseName, @CreatedBy, GETDATE());
+
+    SELECT SCOPE_IDENTITY() AS NewCourseId;
+END
+GO
